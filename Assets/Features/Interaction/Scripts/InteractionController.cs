af7c87de2f093aad8a6da8054bf1c96050d3a3cb
Jordan Cogswell <jordancog94@gmail.com>
2026-01-31 10:48:52 -0500

    Basic first person controls + interactibles

---
+++ Assets/Features/Interaction/Scripts/InteractionController.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using TMPro;
+
+public class InteractionController : MonoBehaviour
+{
+	[Header("Settings")]
+	[SerializeField] private float _interactionRange = 3f;
+	[SerializeField] private LayerMask _interactableLayer;
+	[SerializeField] private KeyCode _interactionKey = KeyCode.F;
+
+	[Header("References")]
+	[SerializeField] private Camera _camera;
+	[SerializeField] private TMP_Text _interactionPromptUI;
+
+	private IInteractable _currentInteractable;
+
+	private void Update()
+	{
+		PerformInteractionCheck();
+
+		if (Input.GetKeyDown(_interactionKey))
+		{
+			if (_currentInteractable != null)
+			{
+				_currentInteractable.Interact();
+			}
+		}
+	}
+
+	private void PerformInteractionCheck()
+	{
+		if (_camera == null)
+		{
+			return;
+		}
+
+		Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, _interactionRange, _interactableLayer))
+		{
+			IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+			if (interactable != null)
+			{
+				if (_currentInteractable != interactable)
+				{
+					ClearCurrentInteractable();
+					_currentInteractable = interactable;
+					_currentInteractable.OnHoverEnter();
+					
+					if (_interactionPromptUI != null)
+					{
+                        _interactionPromptUI.text = "Press " + _interactionKey.ToString() + " to interact";
+						_interactionPromptUI.gameObject.SetActive(true);
+					}
+				}
+				
+				return;
+			}
+		}
+
+		ClearCurrentInteractable();
+	}
+
+	private void ClearCurrentInteractable()
+	{
+		if (_currentInteractable != null)
+		{
+			_currentInteractable.OnHoverExit();
+			_currentInteractable = null;
+			
+			if (_interactionPromptUI != null)
+			{
+				_interactionPromptUI.gameObject.SetActive(false);
+			}
+		}
+	}
+}
---
+++ Assets/Features/Interaction/Scripts/SimpleInteractable.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class SimpleInteractable : MonoBehaviour, IInteractable
+{
+	[Header("Settings")]
+	[SerializeField] private string _message = "Interacted!";
+	[SerializeField] private Material _highlightMaterial;
+
+	[Header("References")]
+	[SerializeField] private Renderer _renderer;
+
+	private Material _originalMaterial;
+
+	protected virtual void Start()
+	{
+		if (_renderer == null)
+		{
+			_renderer = GetComponent<Renderer>();
+		}
+
+		if (_renderer != null)
+		{
+			_originalMaterial = _renderer.sharedMaterial;
+		}
+	}
+
+	public virtual void Interact()
+	{
+		Debug.Log($"{gameObject.name}: {_message}");
+	}
+
+	public virtual void OnHoverEnter()
+	{
+		if (_renderer != null && _highlightMaterial != null)
+		{
+			_renderer.sharedMaterial = _highlightMaterial;
+		}
+	}
+
+	public virtual void OnHoverExit()
+	{
+		if (_renderer != null)
+		{
+			_renderer.sharedMaterial = _originalMaterial;
+		}
+	}
+}
