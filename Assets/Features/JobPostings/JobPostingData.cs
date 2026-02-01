97901666a75a9d8fa827abf97bbd61b5a3e72cf6
FelixLamarche <felixlamarche28@gmail.com>
2026-02-01 12:20:55 -0500

    Update JobPostingData.cs

--- Assets/Features/JobPostings/JobPostingData.cs
+++ Assets/Features/JobPostings/JobPostingData.cs
@@ -14,7 +14,6 @@ public enum JobType
 [CreateAssetMenu(fileName = "JobPostingData", menuName = "JobPostings/JobPostingData")]
 public class JobPostingData : ScriptableObject
 {
-    [SerializeField] private GUID _jobID;
 	[SerializeField] private string _jobTitle;
     [SerializeField] private string _companyName;
     [SerializeField] private string _jobDescription;
@@ -23,7 +22,6 @@ public class JobPostingData : ScriptableObject
     [SerializeField, Tooltip("To tag specific data")] private string _dataTag = string.Empty;
     [SerializeField] private JobType _jobType;
 
-    public GUID JobID => _jobID;
     public string JobTitle => _jobTitle;
     public string CompanyName => _companyName;
     public string JobDescription => _jobDescription;
@@ -31,9 +29,4 @@ public class JobPostingData : ScriptableObject
     public string DataTag => _dataTag;
     public JobType JobType => _jobType;
 
-	void OnEnable()
-    {
-        if(JobID.Empty())
-			_jobID = GUID.Generate();
-    }
 }
