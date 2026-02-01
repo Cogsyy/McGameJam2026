b1eff5f6bbd9d331a6c5921995ba65cfd8cd8371
Jordan Cogswell <jordancog94@gmail.com>
2026-02-01 10:54:07 -0500

    Add semi functional pause menu

--- Assets/Features/PauseMenu/PauseMenu.cs
+++ Assets/Features/PauseMenu/PauseMenu.cs
@@ -8,7 +8,7 @@ public class PauseMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _pauseMenu.enabled = false;
     }
 
     // Update is called once per frame
