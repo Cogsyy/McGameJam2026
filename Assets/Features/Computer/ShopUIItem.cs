25dcfea1df49a8b28c7142d0d7ae733e047b9d7f
Jordan Cogswell <jordancog94@gmail.com>
2026-01-31 19:22:57 -0500

    Basic shop page

---
+++ Assets/Features/Computer/ShopUIItem.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopUIItem : MonoBehaviour
+{
+	[Header("References")]
+	[SerializeField] private Image _iconImage;
+	[SerializeField] private TMP_Text _priceText;
+	[SerializeField] private Button _buyButton;
+
+	private ShopItem _currentItem;
+	private ShopPage _owner;
+
+	public void Setup(ShopPage owner, ShopItem item)
+	{
+		_owner = owner;
+		_currentItem = item;
+
+		if (_iconImage != null) _iconImage.sprite = item.Icon;
+		if (_priceText != null) _priceText.text = "$" + item.Price.ToString();
+
+		_buyButton.onClick.RemoveAllListeners();
+		_buyButton.onClick.AddListener(OnBuyClicked);
+	}
+
+	private void OnBuyClicked()
+	{
+		if (_owner != null && _currentItem != null)
+		{
+			if (_owner.TryPurchaseItem(_currentItem))
+            {
+                gameObject.SetActive(false);
+            }
+		}
+	}
+}
